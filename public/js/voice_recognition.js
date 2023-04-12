0bc89d44078acf5b990ddfc692c10f3f4f1e0d24
santhosh-4000 <santhoshadabala6499@gmail.com>
2023-04-12 11:15:09 +0530

    extend browser voice-recognition to multiple input fields

--- public/js/voice_recognition.js
+++ public/js/voice_recognition.js
@@ -1,6 +1,17 @@
-  if ("webkitSpeechRecognition" in window) {
+    var dict = {
+      mic_medicine_name: "medicine_name",
+      mic_medicine_conc: "medicine_conc",
+      mic_medicine_dosg: "medicine_dosg",
+      mic_medicine_quantity: "medicine_quantity",
+      mic_prescription_notes: "prescription_notes",
+      mic_patient_symptoms: "patient_symptoms",
+      mic_patient_diagnosis: "patient_diagnosis"
+    }
+    
+if ("webkitSpeechRecognition" in window) {
     let speechRecognition = new webkitSpeechRecognition();
   
+    var input_id = "";
     let final_transcript = "";
     let recognizing = false
     // Set the properties for the Speech Recognition object
@@ -8,54 +19,44 @@
     speechRecognition.interimResults = true;
     speechRecognition.lang = "en_IN";
   
-    // Callback Function for the onStart Event
     speechRecognition.onstart = (event) => {
       recognizing = true
-      document.querySelector("#mic-medicine-name").setAttribute('class', 'fa-duotone fa-circle-microphon');
     };
     speechRecognition.onerror = () => {
-      document.querySelector("#mic-medicine-name").setAttribute('class', 'fas fa-microphone');
+      
     };
     speechRecognition.onend = () => {
-      document.querySelector("#mic-medicine-name").setAttribute('class', 'fas fa-microphone');
+      
     };
   
     speechRecognition.onresult = (event) => {
-      // Create the interim transcript string locally because we don't want it to persist like final transcript
       let interim_transcript = "";
   
-      // Loop through the results from the speech recognition object.
       for (let i = event.resultIndex; i < event.results.length; ++i) {
-        // If the result item is Final, add it to Final Transcript, Else add it to Interim transcript
         if (event.results[i].isFinal) {
           final_transcript += event.results[i][0].transcript;
         } else {
           interim_transcript += event.results[i][0].transcript;
         }
       }
-  
-      // Set the Final transcript and Interim transcript.
-      document.querySelector("#medicine_name").setAttribute('value', final_transcript + interim_transcript);
+
+      console.log(final_transcript);
+      console.log(input_id);
+      document.querySelector(input_id).value = final_transcript + interim_transcript;
     };
 
     function startButton(event) {
+      input_id = "#" + dict[event.target.id];
+
       if (recognizing) {
         speechRecognition.stop();
         recognizing = false
-        document.querySelector("#medicine_name").setAttribute('value', final_transcript);
-        console.log(final_transcript);
+        document.querySelector(input_id).value = final_transcript;
         return;
       }
       final_transcript = '';
-      // recognition.lang = 'en-IN'
       speechRecognition.start();
-      // ignore_onend = false;
-      // final_span.innerHTML = '';
-      // interim_span.innerHTML = '';
-      // start_img.src = '/intl/en/chrome/assets/common/images/content/mic-slash.gif';
     }
-  } else {
+} else {
     console.log("Speech Recognition Not Available");
   }
-
-  // <i class="fa-duotone fa-circle-microphone"></i>
