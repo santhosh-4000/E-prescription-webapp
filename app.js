dac111fc3a31b890c6cb7bf214027c9c3825bbfb
santhosh-4000 <santhoshadabala6499@gmail.com>
2023-04-14 12:09:34 +0530

    add home page

--- app.js
+++ app.js
@@ -8,20 +8,8 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// var con = mysql.createConnection({
-//     host: "127.0.0.1",
-//     user: "root",
-//     password: "Pavan@2002"
-//   });
-
-  // con.connect(function(err) {
-  //   if (err) throw err;
-  //   console.log("Connected!");
-  // });
-
 app.get("/", function(req, res) {
-    var doctor_id = "h101"; //for testing
-    res.render("home", { doctor_id: doctor_id, doctor_name: 'test', hospital_name: 'test'})
+    res.render("home")
 });
 
 app.listen(3000, function() {
