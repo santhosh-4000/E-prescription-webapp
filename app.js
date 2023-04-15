049895c3458e746408adb9848e3ad2992a435483
santhosh-4000 <santhoshadabala6499@gmail.com>
2023-04-15 11:07:21 +0530

    handle post requests login

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+const MongoDBURI = process.env.MONGO_URI || 'mongodb://localhost:27017/ManualAuth';
+
+mongoose.connect(MongoDBURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(()=>console.log("connected"))
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+});
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store:  MongoStore.create({
+    mongoUrl: MongoDBURI
+  })
+}));
 
 const index = require('./routes/index');
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -27,128 +27,141 @@ router.get('/forgetpass', (req, res, next) => {
 	res.render("forget.ejs");
 });
 
-// router.post('/', (req, res, next) => {
-// 	let personInfo = req.body;
+router.get('/prescribe', (req, res, next) => {
+	res.render("prescribe.ejs");
+});
 
-// 	if (!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf) {
-// 		res.send();
-// 	} else {
-// 		if (personInfo.password == personInfo.passwordConf) {
+router.post('/login', (req, res, next) => {
+	User.findOne({ email: req.body.email }).then((data) => {
+		if (data) {
 
-// 			User.findOne({ email: personInfo.email }, (err, data) => {
-// 				if (!data) {
-// 					let c;
-// 					User.findOne({}, (err, data) => {
+			if (data.password == req.body.password) {
+				req.session.userId = data.unique_id;
+				res.send({ "Success": "Success!" });
+			} else {
+				res.send({ "Success": "Wrong password!" });
+			}
+		} else {
+			res.send({ "Success": "This Email Is not regestered!" });
+		}
+	});
+});
 
-// 						if (data) {
-// 							c = data.unique_id + 1;
-// 						} else {
-// 							c = 1;
-// 						}
+router.post('/', (req, res, next) => {
+	let personInfo = req.body;
 
-// 						let newPerson = new User({
-// 							unique_id: c,
-// 							email: personInfo.email,
-// 							username: personInfo.username,
-// 							password: personInfo.password,
-// 							passwordConf: personInfo.passwordConf
-// 						});
+	if (!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf) {
+		res.send();
+	} else {
+		if (personInfo.password == personInfo.passwordConf) {
 
-// 						newPerson.save((err, Person) => {
-// 							if (err)
-// 								console.log(err);
-// 							else
-// 								console.log('Success');
-// 						});
+			User.findOne({ email: personInfo.email }).then( (data) => {
+				if (!data) {
+					let c;
+					User.findOne({}, null,{ sort: { _id: -1 }, limit: 1}). then( (data) => {
 
-// 					}).sort({ _id: -1 }).limit(1);
-// 					res.send({ "Success": "You are regestered,You can login now." });
-// 				} else {
-// 					res.send({ "Success": "Email is already used." });
-// 				}
+						if (data) {
+							c = data.unique_id + 1;
+						} else {
+							c = 1;
+						}
 
-// 			});
-// 		} else {
-// 			res.send({ "Success": "password is not matched" });
-// 		}
-// 	}
-// });
+						let newPerson = new User({
+							unique_id: c,
+							email: personInfo.email,
+							username: personInfo.username,
+							password: personInfo.password,
+							passwordConf: personInfo.passwordConf
+						});
 
-// router.post('/patient', (req, res, next) => {
-// 	let patientinfo = req.body;
-//      console.log(patientinfo);
-// 			Detail.findOne({ patient_name : patientinfo.patient_name }, (err, data) => {
-// 				if (!data) {
-// 					let c;
-// 					Detail.findOne({}, (err, data) => {
+						newPerson.save().then((Person) => {
+								console.log('Success\n' + Person);
+						});
 
-// 						if (data) {
-// 							c = data.unique_id + 1;
-// 						} else {
-// 							c = 1;
-// 						}
+					});
+					res.send({ "Success": "You are regestered,You can login now." });
+				} else {
+					res.send({ "Success": "Email is already used." });
+				}
 
-// 						let newpatient = new Detail({
-// 							phonenumber: patientinfo.phonenumber,
-// 							patient_name: patientinfo.patient_name,
-// 							age: patientinfo.age,
-// 							gender:patientinfo.gender,
-// 							dateofbirth: patientinfo.dateofbirth,
-// 							healthissue: patientinfo.healthissue,
-// 							symptoms: patientinfo.symptoms,
-// 						});
+			});
+		} else {
+			res.send({ "Success": "password is not matched" });
+		}
+	}
+});
 
-// 						newpatient.save((err, Person) => {
-// 							if (err)
-// 								console.log(err);
-// 							else
-// 								console.log('Success');
-// 						});
+router.get('/profile', (req, res, next) => {
+	User.findOne({ unique_id: req.session.userId }).then( (data) => {
+		if (!data) {
+			res.redirect('/');
+		} else {
+			return res.render('profile.ejs');
+		}
+	});
+});
 
-// 					}).sort({ _id: -1 }).limit(1);
-// 					res.send({ "Success": "You are regestered,You can login now." });
-// 				} else {
-// 					res.send({ "Success": "Email is already used." });
-// 				}
+router.post('/patient', (req, res, next) => {
+	let patientinfo = req.body;
+     console.log(patientinfo);
+			Detail.findOne({ phonenumber: patientinfo.phonenumber }).then( (data) => {
+				if (!data) {
+					let c;
+					Detail.findOne({}, null,{ sort: { _id: -1 }, limit: 1}).then( (data) => {
 
-// 			});
-// 		} 
-// 	);
+						if (data) {
+							c = data.unique_id + 1;
+						} else {
+							c = 1;
+						}
 
+						let newpatient = new Detail({
+							phonenumber: patientinfo.phonenumber,
+							patient_name: patientinfo.patient_name,
+							age: patientinfo.age,
+							gender:patientinfo.gender,
+							dateofbirth: patientinfo.dateofbirth,
+							healthissue: patientinfo.healthissue,
+							symptoms: patientinfo.symptoms,
+						});
 
-// router.get('/login', (req, res, next) => {
-// 	return res.render('login.ejs');
-// });
+						newpatient.save().then((Person) => {
+							console.log('Success\n'+ Person);
+						});
 
+					});
+					res.send({ "Success": "You are regestered,You can login now." });
+				} else {
+					res.send({ "Success": "Email is already used." });
+				}
+
+			});
+		} 
+	);
+
+	router.post('/forgetpass', (req, res, next) => {
+		User.findOne({ email: req.body.email }).then( (data) => {
+			if (!data) {
+				res.send({ "Success": "This Email Is not regestered!" });
+			} else {
+				if (req.body.password == req.body.passwordConf) {
+					data.password = req.body.password;
+					data.passwordConf = req.body.passwordConf;
+	
+					data.save().then((Person) => {
+						console.log('Success\n'+ Person);
+						res.send({ "Success": "Password changed!" });
+					});
+				} else {
+					res.send({ "Success": "Password does not matched! Both Password should be same." });
+				}
+			}
+		});
+	
+	});
 
 // router.get('/home', (req, res, next) => {
 // 	return res.render('home.ejs');
-// });
-
-// router.post('/login', (req, res, next) => {
-// 	User.findOne({ email: req.body.email }, (err, data) => {
-// 		if (data) {
-
-// 			if (data.password == req.body.password) {
-// 				req.session.userId = data.unique_id;
-// 				res.send({ "Success": "Success!" });
-// 			} else {
-// 				res.send({ "Success": "Wrong password!" });
-// 			}
-// 		} else {
-// 			res.send({ "Success": "This Email Is not regestered!" });
-// 		}
-// 	});
-// });
-
-// router.get('/profile', (req, res, next) => {
-// 	User.findOne({ unique_id: req.session.userId }, (err, data) => {
-// 		if (!data) {
-// 			res.redirect('/');
-// 		} else {
-// 			return res.render('profile.ejs');
-// 		}
-// 	});
 // });
 
 // router.post('/profile', (req, res, next) => {
@@ -156,11 +169,8 @@ router.get('/forgetpass', (req, res, next) => {
 
 // 	let newPerson = new User(patient);
 
-// 	newPerson.save((err, Person) => {
-// 		if (err)
-// 			console.log(err);
-// 		else
-// 			console.log('Success');
+// 	newPerson.save().then((Person) => {
+// 			console.log('Success\n'+ Person);
 // 	});
 
 // 	res.send("Done")
@@ -177,30 +187,6 @@ router.get('/forgetpass', (req, res, next) => {
 // 			}
 // 		});
 // 	}
-// });
-
-// router.post('/forgetpass', (req, res, next) => {
-// 	User.findOne({ email: req.body.email }, (err, data) => {
-// 		if (!data) {
-// 			res.send({ "Success": "This Email Is not regestered!" });
-// 		} else {
-// 			if (req.body.password == req.body.passwordConf) {
-// 				data.password = req.body.password;
-// 				data.passwordConf = req.body.passwordConf;
-
-// 				data.save((err, Person) => {
-// 					if (err)
-// 						console.log(err);
-// 					else
-// 						console.log('Success');
-// 					res.send({ "Success": "Password changed!" });
-// 				});
-// 			} else {
-// 				res.send({ "Success": "Password does not matched! Both Password should be same." });
-// 			}
-// 		}
-// 	});
-
 // });
 
 module.exports = router;
